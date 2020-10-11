29075f9abc9ebfcb588700878daa6e1af7bcc34e
Mai Pham <maitphm@gmail.com>
2020-10-11 04:15:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   
     squared_numbers = []
 	  numbers.each do |num|
-      squared << (num ** 2)
+      squared_numbers << (num ** 2)
   end
   return squared_numbers
   
